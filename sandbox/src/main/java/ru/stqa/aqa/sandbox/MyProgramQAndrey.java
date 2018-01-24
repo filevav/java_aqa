9f587d7a7eed05be7728aed2807e6f3cb36230f0
filevav <filevav@gmail.com>
2018-01-24 22:05:10 +0200

    Переменные

--- sandbox/src/main/java/ru/stqa/aqa/sandbox/MyProgramQAndrey.java
+++ sandbox/src/main/java/ru/stqa/aqa/sandbox/MyProgramQAndrey.java
@@ -3,7 +3,11 @@ package ru.stqa.aqa.sandbox;
 public class MyProgramQAndrey {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello, " + "world!");
+
+		int l = 8;
+		int s = l * l;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
 	}
 
 }
