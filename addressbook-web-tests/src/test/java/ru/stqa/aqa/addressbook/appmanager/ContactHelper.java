64af38b935f8c2c10ce53447f86654d5015698d2
Andrey.QA <filevav@gmail.com>
2018-03-13 12:12:56 +0200

    ContactCreationTests fix

--- addressbook-web-tests/src/test/java/ru/stqa/aqa/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/aqa/addressbook/appmanager/ContactHelper.java
@@ -20,7 +20,9 @@ public class ContactHelper extends HelperBase {
         type(By.name("lastname"), contactData.getLastName());
 
         if (creation) {
-            new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+            if (contactData.getGroup() != null) {
+                new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+            }
         } else {
             Assert.assertFalse(isElementPresent(By.name("new_group")));
         }
