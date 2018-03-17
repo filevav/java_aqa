ad3c9527c3c40052f70f51ab5015098ec6f1ce6d
filevav <filevav@gmail.com>
2018-03-17 23:15:20 +0200

    6.6. Формат XML

--- addressbook-web-tests/src/test/java/ru/stqa/aqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/aqa/addressbook/model/GroupData.java
@@ -1,8 +1,12 @@
 package ru.stqa.aqa.addressbook.model;
 
-import java.util.Objects;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
+@XStreamAlias("group")
 
 public class GroupData {
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;
     private String name;
     private String header;
