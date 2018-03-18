8df821438f993505c85e634fd614dca11c867d66
filevav <filevav@gmail.com>
2018-03-18 13:27:35 +0200

    6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.aqa.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.aqa.addressbook.model.GroupData;
@@ -17,6 +19,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase{
+
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
