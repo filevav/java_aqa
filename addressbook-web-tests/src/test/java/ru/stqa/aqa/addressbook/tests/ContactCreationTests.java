107a9dc7adca89feb401213388dfd829f7a73ea3
filevav <filevav@gmail.com>
2018-03-24 13:17:08 +0200

    ДЗ №13 v.1.1 (6.4)

--- addressbook-web-tests/src/test/java/ru/stqa/aqa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/aqa/addressbook/tests/ContactCreationTests.java
@@ -1,27 +1,34 @@
 package ru.stqa.aqa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.aqa.addressbook.model.ContactData;
 import ru.stqa.aqa.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation () {
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        File photo = new File("src/test/resources/nightlights.jpeg");
+        list.add(new Object[] {new ContactData().withFirstName("TestFN1").withLastName("TestLN1").withMobilePhone("80661876181").withAddress("ul. Pi1").withPhoto(photo)});
+        list.add(new Object[] {new ContactData().withFirstName("TestFN2").withLastName("TestLN2").withMobilePhone("80661876182").withAddress("ul. Pi2").withPhoto(photo)});
+        list.add(new Object[] {new ContactData().withFirstName("TestFN3").withLastName("TestLN3").withMobilePhone("80661876183").withAddress("ul. Pi3").withPhoto(photo)});
+        return list.iterator();
+    }
+
+    @Test (dataProvider = "validContacts")
+    public void testContactCreation (ContactData contact) {
         Contacts before = app.contact().all();
         app.goTo().addNewPage();
-        File photo = new File("src/test/resources/nightlights.jpeg");
-        ContactData contact = new ContactData()
-                .withFirstName("TestFN")
-                .withLastName("TestLN")
-                .withMobilePhone("222")
-                .withAddress("ul. Pi")
-                .withPhoto(photo);
         app.contact().create(contact, true);
         app.contact().homePage();
         Contacts after = app.contact().all();
@@ -32,16 +39,6 @@ public class ContactCreationTests extends TestBase {
     }
 
     @Test (enabled = false)
-    public void testCurrentDir() {
-        File currentDir = new File(".");
-        System.out.println(currentDir.getAbsolutePath());
-        File photo = new File("src/test/resources/nightlights.jpeg");
-        System.out.println(currentDir.getAbsolutePath());
-        System.out.println(photo.exists());
-
-    }
-
-    @Test
     public void testBadContactCreation () {
         Contacts before = app.contact().all();
         app.goTo().addNewPage();
@@ -54,5 +51,15 @@ public class ContactCreationTests extends TestBase {
         assertThat(app.contact().count(), equalTo(before.size()));
         Contacts after = app.contact().all();
         assertThat(after, equalTo(before));
+    }
+
+    @Test (enabled = false)
+    public void testCurrentDir() {
+        File currentDir = new File(".");
+        System.out.println(currentDir.getAbsolutePath());
+        File photo = new File("src/test/resources/nightlights.jpeg");
+        System.out.println(currentDir.getAbsolutePath());
+        System.out.println(photo.exists());
+
     }
 }
