7ea26d303434522503fc8f0ac4188098f8e52841
Andrey.QA <filevav@gmail.com>
2018-04-03 17:41:09 +0300

    8.4. Lazy init

--- mantis-tests/src/test/java/ru/stqa/aqa/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/aqa/mantis/appmanager/ApplicationManager.java
@@ -16,9 +16,10 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -29,22 +30,43 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-        if (Objects.equals(browser, BrowserType.FIREFOX)) {
-            System.setProperty("webdriver.gecko.driver","C:\\Tools\\geckodriver.exe");
-            wd = new FirefoxDriver();
-        } else if (Objects.equals(browser, BrowserType.CHROME)) {
-            //System.setProperty("webdriver.chrome.driver","C:\\Tools\\chromedriver.exe");
-            wd = new ChromeDriver();
-        } else if (Objects.equals(browser, BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null){
+            if (Objects.equals(browser, BrowserType.FIREFOX)) {
+                System.setProperty("webdriver.gecko.driver","C:\\Tools\\geckodriver.exe");
+                wd = new FirefoxDriver();
+            } else if (Objects.equals(browser, BrowserType.CHROME)) {
+                System.setProperty("webdriver.chrome.driver","C:\\Tools\\chromedriver.exe");
+                wd = new ChromeDriver();
+            } else if (Objects.equals(browser, BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
 }
