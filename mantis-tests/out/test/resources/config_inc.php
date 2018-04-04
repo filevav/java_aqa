054d86377de56085cbd14290bf6cf1f552092242
Andrey.QA <filevav@gmail.com>
2018-04-04 17:41:37 +0300

    8.6

---
+++ mantis-tests/out/test/resources/config_inc.php
@@ -0,0 +1,14 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
+$g_smpt_host              = 'localhost'
+
+$g_default_timezone       = 'Europe/Berlin';
+$g_crypto_master_salt     = '94szLG7P8A9SFJ/Zpox5f5/KhHBmXyMHKGs1oGPglFE=';
+
+$g_signup_use_captcha     = OFF;
