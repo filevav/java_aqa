948fd764fa3f7ae2c51a6375becf36c62b066b6a
Andrey.QA <filevav@gmail.com>
2018-05-11 18:00:23 +0300

    Переход на старую джиру (1.2.19)

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -1,14 +1,9 @@
 <?php
 $g_hostname               = 'localhost';
 $g_db_type                = 'mysqli';
-$g_database_name          = 'bugtracker';
+$g_database_name          = 'bugtrackerm';
 $g_db_username            = 'root';
 $g_db_password            = '';
-
-$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
-$g_smpt_host              = 'localhost'
-
-$g_default_timezone       = 'Europe/Berlin';
-$g_crypto_master_salt     = '94szLG7P8A9SFJ/Zpox5f5/KhHBmXyMHKGs1oGPglFE=';
-
 $g_signup_use_captcha     = OFF;
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
+$g_smtp_host              = 'localhost';
